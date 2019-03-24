1fcb58bd4ea3de893ca9318e81f55ad91d7eddb3
J. Singleton <jennifer.singleton@gmail.com>
2019-03-24 17:13:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,6 +7,10 @@ class Application
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
 
+    resp.write <<-TXT
+    | #{num_1} | #{num_2} | #{num_3} |
+    TXT
+
     if num_1 == num_2 && num_2 == num_3
       resp.write "You Win"
     else
